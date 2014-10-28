4ff1cea3572410b56ead07ce568dd204df800f04
Ramon Houtsma <ramon@houtsma.net>
2014-10-28 11:17:10 +0100

    duck.py WIP

--- duckhunt-master/game/duck.py
+++ duckhunt-master/game/duck.py
@@ -1,1 +1,11 @@
-sw
+import os
+import pygame
+
+ducklist = ['media/images/duck1.png','media/images/duck2.png','media/images/duck3.png']
+screen = pygame.display.set_mode((800, 500))
+while(true)
+    for im in ducklist
+        image = pygame.image.load('im')
+        screen.fill((255,255,255))
+        screen.blit(image, (20,20))
+        pygame.display.flip()
