8960554c4eb4cc78e42509035b6d05539217019d
joerikock <joerikock@live.nl>
2014-10-28 11:16:23 +0100

    game folder

--- duckhunt-master/controller.py
+++ duckhunt-master/controller.py
@@ -3,13 +3,10 @@ import pygame
 import pygame.transform
 
 # Game parameters
-FRAMES_PER_SEC = 50
-white = (255, 64, 64)
 screen = pygame.display.set_mode((800, 500))
 image = pygame.image.load('media/background.jpg')
 
 # Initialize pygame before importing modules
-pygame.mixer.pre_init(44100, -16, 2, 1024)
 pygame.init()
 pygame.display.set_caption("Duck Hunt")
 pygame.mouse.set_visible(False)
@@ -17,7 +14,6 @@ pygame.mouse.set_visible(False)
 class Controller(object):
     def __init__(self):
         self.running = True
-        screen.fill((white))
 
     def execute(self):
         screen.blit(image, (0, 0))
