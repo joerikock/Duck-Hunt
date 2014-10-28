5235ae312a397b4e8b6d296958b030d001636aec
joerikock <joerikock@live.nl>
2014-10-28 11:11:17 +0100

    Controller runs!

--- duckhunt-master/controller.py
+++ duckhunt-master/controller.py
@@ -5,32 +5,32 @@ import pygame.transform
 # Game parameters
 FRAMES_PER_SEC = 50
 white = (255, 64, 64)
+screen = pygame.display.set_mode((800, 500))
+image = pygame.image.load('media/background.jpg')
 
 # Initialize pygame before importing modules
 pygame.mixer.pre_init(44100, -16, 2, 1024)
 pygame.init()
 pygame.display.set_caption("Duck Hunt")
 pygame.mouse.set_visible(False)
-image = pygame.image.load('media', 'background.jpg')
 
 class Controller(object):
     def __init__(self):
         self.running = True
-        self.clock = pygame.time.Clock()
-        screen = pygame.display.set_mode((800, 500))
         screen.fill((white))
 
-    def loop(self):
-        self.clock.tick(FRAMES_PER_SEC)
-
     def execute(self):
-        while self.running:
-            screen.blit(image, (0, 0))
-            pygame.display.flip()
-            if event.type == pygame.QUIT:
-                self.running = False
+        screen.blit(image, (0, 0))
+        try:
+            while self.running:
+                pygame.display.flip()
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        self.running = False
+            pygame.quit()
+        except SystemExit:
+            pygame.quit()
        
-
 if __name__ == "__main__":
     controller = Controller()
     controller.execute()
